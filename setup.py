80d4bf533246a6e976c093951f3be2516a550b9d
jaybfn <jayesh.bfn@gmail.com>
2023-11-03 21:29:43 +0100

    build!

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 
 setup(
     name="pynanopore",
-    version="1.0.4",
+    version="1.0.5",
     packages=['pynanopore'],
     long_description = long_description,
     long_description_content_type = "text/markdown",
